5f8a857d5989296beab843799827f6db92ecdc03
Olga Patrakova <olgapatrakova@outlook.com>
2020-03-26 14:52:55 -0700

    Array intersection complete

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,17 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  return nil if list1 == nil || list2 == nil
+
+  hash = {}
+  list1.each do |element|
+    hash[element] = true
+  end
+
+  result = []
+  list2.each do |number|
+    if hash[number]
+      result << number
+    end
+  end
+
+  return result
 end
