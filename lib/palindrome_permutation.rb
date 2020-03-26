7e0766c0dbbbe57e0b8b70a0e25befa75900682f
Olga Patrakova <olgapatrakova@outlook.com>
2020-03-26 15:36:52 -0700

    palindrome_permutation complete

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,22 @@
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  return false if string.match(/[^a-zA-Z]/)
+  return true if string.length == 0
+  hash = {}
+  string.length.times do |i|
+    if hash[string[i]]
+      hash[string[i]] += 1
+    else
+      hash[string[i]] = 1
+    end
+  end
+
+  odd = 0
+  hash.values.each do |integer|
+    if integer.odd?
+      odd += 1
+    end
+  end
+  
+  return false if odd > 1
 end
--- lib/permutations.rb
+++ lib/permutations.rb
@@ -3,19 +3,13 @@ def permutations?(string1, string2)
   return false if string1.length != string2.length
   return true if string1 == string2
 
-  #Make an array from the 1st string
-  array = []
-  string1.length.times do |i|
-    array[i] = string1[i]
-  end
-
-  # Make a hash from the array, taking care of duplicates 
+  # Make a hash from a string, taking care of duplicates 
   hash = {}
-  array.each do |element|
-    if hash[element]
-      hash[element] += 1
+  string1.length.times do |i|
+    if hash[string1[i]]
+      hash[string1[i]] += 1
     else
-      hash[element] = 1
+      hash[string1[i]] = 1
     end
   end
 
